b99663fed1699d1dfbae93b7dc32d148f10b3d66
Thedum2 <asdf74591@gmail.com>
2025-12-28 16:13:11 +0900

    feat:config수정

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,7 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
+const API_STAGE = import.meta.env.VITE_API_STAGE;
+
+export const API_BASE_URL = `${API_URL}/${API_STAGE}`;
 export const SHOULD_LOG = (typeof import.meta !== "undefined" && import.meta.env.MODE !== "production")
 export const IS_DEV = import.meta.env.MODE === "development";
 export const API_TIMEOUT_MS = 15000;
