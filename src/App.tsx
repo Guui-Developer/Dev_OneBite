f45f503e7abc6a17aa2e1190723ecbefedccd10f
Thedum2 <asdf74591@gmail.com>
2026-01-02 21:48:26 +0900

    feat: 개인정보 처리방침 업로드

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import History from './pages/History'
 import Category from './pages/Category'
 import Learn from './pages/Learn'
 import Terms from './pages/Terms'
+import Privacy from './pages/Privacy'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/learn" element={<Learn />} />
             <Route path="/history" element={<History />} />
             <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
           </Routes>
         </BrowserRouter>
       </div>
